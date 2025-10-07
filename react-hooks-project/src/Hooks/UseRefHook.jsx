043af26131a5c6e505b5102fcd519b9d894571f3
imnisi <nishantkrbihta01@gmail.com>
2025-10-08 02:13:55 +0530

    more about useEffect hooks

--- react-hooks-project/src/Hooks/UseRefHook.jsx
+++ react-hooks-project/src/Hooks/UseRefHook.jsx
@@ -17,6 +17,7 @@ function UseRefHook() {
   const count = useRef(0);
   useEffect(() => {
     count.current = count.current + 1;
+    console.log("Render Count:", count.current);
   });
 
   return (
